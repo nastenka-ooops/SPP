f817f8217fbb6fdc364564afe7783136b40eaf67
Anasty Brutskaya <anastasia.brutskaya@digitalchief.ru>
2025-02-10 16:20:32 +0300

    lapa 1

---
+++ lapa1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace lapa1;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string? s = Console.ReadLine();
+        int threadCount = s == null ? 0 : int.Parse(s);
+        TaskQueue taskQueue = new TaskQueue(threadCount);
+
+        for (int i = 0; i < 10; i++)
+        {
+            int taskNumber = i;
+            taskQueue.AddTask(() =>
+            {
+                Console.WriteLine($"Running task {taskNumber} on thread {Thread.CurrentThread.ManagedThreadId}");
+            });
+        }
+        
+        Thread.Sleep(2000);
+        taskQueue.Stop();
+    }
+}
---
+++ lapa1/TaskQueue.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Concurrent;
+
+namespace lapa1;
+
+public delegate void TaskDelegate();
+
+public class TaskQueue
+{
+    private Thread[] threads;
+    private BlockingCollection<TaskDelegate> taskQueue;
+    private bool running;
+
+    public TaskQueue(int threadCount)
+    {
+        threads = new Thread[threadCount];
+        taskQueue = new BlockingCollection<TaskDelegate>();
+        running = true;
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            threads[i] = new Thread(RunTasks)
+            {
+                //object initializer
+                IsBackground = true
+            };
+            threads[i].Start();
+        }
+    }
+
+    private void RunTasks()
+    {
+        foreach (var task in taskQueue.GetConsumingEnumerable())
+        {
+            try
+            {
+                task();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка выполнения задачи: {ex.Message}");
+            }
+        }
+    }
+
+    public void AddTask(TaskDelegate task)
+    {
+        if (!running)
+        {
+            throw new InvalidOperationException("Thread pool is stopped.");
+        }
+
+        taskQueue.Add(task);
+    }
+
+    public void Stop()
+    {
+        running = false;
+        taskQueue.CompleteAdding();
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+    }
+}
